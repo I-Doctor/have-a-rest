c4a366debc34a5cf21de8fe0cd85577555756f80
I-Doctor <ZK14115@163.com>
2019-04-17 22:34:45 +0800

    change a litte in readme file and py file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # have-a-rest
-This is a simple output program which will let you have a rest during your coding
+This is a simple output program which will let you have a rest during your coding.
+
+You need tqdm to run the code.
--- have-a-rest.py
+++ have-a-rest.py
@@ -1,21 +1,31 @@
+# Copyright (c) 2019 ZK14115@163.com
+# This is a relax program
+
 from tqdm import tqdm
 import time
+import argparse
+
 N = int(1e9)
 T = 1e-2
 MAX_LEN = 100
 
+
 def parse_args():
-    import argparse
+
     parser = argparse.ArgumentParser(description='i really wanna have a rest.')
     parser.add_argument('-n', '--iters', type=int, default=N, help='rest iters.')
     parser.add_argument('-f', '--frequency', type=float, default=1/T, help='rest frequency per iter.')
     args = parser.parse_args()
+
     return args
 
+
 def have_a_rest():
+
     args = parse_args()
     str_ = ''
     for idx in tqdm(range(args.iters)):
+
         str_ = str_ + '  '
         if len(str_) > MAX_LEN:
             str_ = str_[MAX_LEN:]
@@ -26,5 +36,9 @@ def have_a_rest():
             print(str_to_print + str_to_print)
         time.sleep(1.0 / args.frequency)
 
+
+
 if __name__ == '__main__':
+
     have_a_rest()
+
